3fffd8fe663a8c7727333fcc7173fe39a5e7bbe6
hb <hb@aroma-project.org>
2016-01-25 08:30:08 -0800

    Added is.matrix(), is.array(), as.vector() and as.matrix() for list environments [#27]

--- R/listenv.R
+++ R/listenv.R
@@ -898,51 +898,6 @@ unlist.listenv <- function(x, recursive=TRUE, use.names=TRUE) {
   }
 }
 
-#' @export
-dim.listenv <- function(x) attr(x, "dim.")
-
-#' @export
-`dim<-.listenv` <- function(x, value) {
-  n <- length(x)
-  if (!is.null(value)) {
-    names <- names(value)
-    value <- as.integer(value)
-    p <- prod(as.double(value))
-    if (p != n) {
-      stop(sprintf("dims [product %d] do not match the length of object [%d]", p, n))
-    }
-    names(value) <- names
-  }
-
-  ## Always remove "dimnames" and "names" attributes, cf. help("dim")
-  dimnames(x) <- NULL
-  names(x) <- NULL
-
-  attr(x, "dim.") <- value
-  x
-}
-
-
-#' @export
-dimnames.listenv <- function(x) attr(x, "dimnames.")
-
-#' @export
-`dimnames<-.listenv` <- function(x, value) {
-  dim <- dim(x)
-  if (is.null(dim) && !is.null(value)) {
-    stop("'dimnames' applied to non-array")
-  }
-  for (kk in seq_along(dim)) {
-    names <- value[[kk]]
-    if (is.null(names)) next
-    n <- length(names)
-    if (n != dim[kk]) {
-      stop(sprintf("length of 'dimnames' [%d] not equal to array extent", kk))
-    }
-  }
-  attr(x, "dimnames.") <- value
-  x
-}
 
 #' @export
 #' @method all.equal listenv
---
+++ tests/as.vector.R
@@ -0,0 +1,49 @@
+library("listenv")
+
+dims <- list(3, c(3,1), c(2,3), c(2,3,4))
+
+for (dim in dims) {
+  x <- as.list(1:prod(dim))
+  if (length(dim) > 1) {
+    dim(x) <- dim
+    dimnames(x) <- lapply(dim, FUN=function(n) letters[seq_len(n)])
+  }
+  y <- as.listenv(x)
+
+##  stopifnot(is.vector(x) == is.vector(y))
+  stopifnot(is.matrix(x) == is.matrix(y))
+  stopifnot(is.array(x) == is.array(y))
+
+  for (mode in c("any", "list", "logical", "integer", "double", "complex", "character", "raw")) {
+    message("mode: ", mode)
+
+    ## as.vector()
+    y <- as.listenv(x)
+    vx <- as.vector(x, mode=mode)
+    print(vx)
+    vy <- as.vector(y, mode=mode)
+    stopifnot(identical(vy, vx))
+    stopifnot(is.matrix(vx) == is.matrix(vy))
+    stopifnot(is.array(vx) == is.array(vy))
+
+    ## as.matrix()
+    y <- as.listenv(x)
+    mx <- as.matrix(x)
+    print(mx)
+    my <- as.matrix(y)
+    stopifnot(identical(dim(my), dim(mx)))
+    stopifnot(identical(dimnames(my), dimnames(mx)))
+    stopifnot(is.matrix(mx) == is.matrix(my))
+    stopifnot(is.array(mx) == is.array(my))
+
+    ## as.array()
+    y <- as.listenv(x)
+    ax <- as.array(x)
+    print(ax)
+    ay <- as.array(y)
+    stopifnot(identical(dim(ay), dim(ax)))
+    stopifnot(identical(dimnames(ay), dimnames(ax)))
+    stopifnot(is.matrix(ax) == is.matrix(ay))
+    stopifnot(is.array(ax) == is.array(ay))
+  }
+}
