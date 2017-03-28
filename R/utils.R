7a50bd4262bc925018de31f07b6c3be57e02247b
hb <hb@aroma-project.org>
2017-03-28 12:21:59 -0700

    STYLE: snake_case internal hpaste()

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
 ## From R.utils 2.0.2 (2015-05-23)
-hpaste <- function(..., sep="", collapse=", ", lastCollapse=NULL,
-                   maxHead=if (missing(lastCollapse)) 3 else Inf,
-                   maxTail=if (is.finite(maxHead)) 1 else Inf,
+hpaste <- function(..., sep="", collapse=", ", last_collapse=NULL,
+                   max_head=if (missing(last_collapse)) 3 else Inf,
+                   max_tail=if (is.finite(max_head)) 1 else Inf,
                    abbreviate="...") {
-  maxHead <- as.double(maxHead)
-  maxTail <- as.double(maxTail)
-  if (is.null(lastCollapse)) lastCollapse <- collapse
+  max_head <- as.double(max_head)
+  max_tail <- as.double(max_tail)
+  if (is.null(last_collapse)) last_collapse <- collapse
 
   # Build vector 'x'
   x <- paste(..., sep = sep)
@@ -16,19 +16,19 @@ hpaste <- function(..., sep="", collapse=", ", lastCollapse=NULL,
   if (is.null(collapse)) return(x)
 
   # Abbreviate?
-  if (n > maxHead + maxTail + 1) {
-    head <- x[seq_len(maxHead)]
-    tail <- rev(rev(x)[seq_len(maxTail)])
+  if (n > max_head + max_tail + 1) {
+    head <- x[seq_len(max_head)]
+    tail <- rev(rev(x)[seq_len(max_tail)])
     x <- c(head, abbreviate, tail)
     n <- length(x)
   }
 
   if (!is.null(collapse) && n > 1) {
-    if (lastCollapse == collapse) {
+    if (last_collapse == collapse) {
       x <- paste(x, collapse = collapse)
     } else {
       x_head <- paste(x[1:(n - 1)], collapse = collapse)
-      x <- paste(x_head, x[n], sep = lastCollapse)
+      x <- paste(x_head, x[n], sep = last_collapse)
     }
   }
 
