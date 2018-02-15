fef17f478ea8efbf492aad83d713abdf790a0fcf
hb <hb@aroma-project.org>
2018-02-15 15:55:50 -0800

    TESTS: Add simple aperm() for listenv to test code [#26]

--- tests/lapply.R
+++ tests/lapply.R
@@ -24,8 +24,15 @@ colnames(x) <- LETTERS[seq_len(ncol(x))]
 y <- as.listenv(x)
 
 z0 <- apply(x, MARGIN = 1L, FUN = function(x) sum(unlist(x)))
-## FIXME: Requires aperm()
-# z1 <- apply(y, MARGIN = 1L, FUN = function(x) sum(unlist(x)))
-# stopifnot(identical(z1, z0))
+
+## FIXME: Implement proper aperm() for listenv
+aperm.listenv <- function(a, ...) {
+  a <- as.list(a)
+  a <- aperm(a, ...)
+  as.listenv(a)
+}
+
+z1 <- apply(y, MARGIN = 1L, FUN = function(x) sum(unlist(x)))
+stopifnot(identical(z1, z0))
 
 message("*** apply() ... DONE")
